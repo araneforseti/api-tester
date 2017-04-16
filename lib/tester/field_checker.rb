3148d14abcb0d60e257b41b7538c755c836ff2b8
Sarah <sarahforseti@gmail.com>
2017-04-16 17:02:46 -0500

    Adding some temporary debugging info

--- lib/tester/field_checker.rb
+++ lib/tester/field_checker.rb
@@ -1,6 +1,7 @@
 class FieldChecker
   def is_field_in_hash field, hash
     if hash.empty?
+      puts "Missing #{field.name}"
       return false
     end
 
@@ -12,6 +13,7 @@ class FieldChecker
       if field.has_subfields?
         field.fields.each do |subfield|
           if !is_field_in_hash(subfield, hash[field.name])
+            puts "Missing #{field.name}.#{subfield.name}"
             return false
           end
         end
