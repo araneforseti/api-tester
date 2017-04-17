bd81228d1e6fb83095c7f7bb2f255a13022d571d
Sarah <sarahforseti@gmail.com>
2017-04-16 20:25:32 -0500

    Adding array handling into field checker

--- lib/tester/field_checker.rb
+++ lib/tester/field_checker.rb
@@ -11,16 +11,56 @@ class FieldChecker
       end
 
       if field.has_subfields?
-        field.fields.each do |subfield|
-          if !is_field_in_hash(subfield, hash[field.name])
-            puts "Missing #{field.name}.#{subfield.name}"
-            return false
-          end
+        if !check_based_on_type(field, hash)
+          return false
         end
       end
       return true
     end
     puts "Missing #{field.name}"
     false
+  end
+
+  def check_subfields field
+
+  end
+
+  def check_object_subfields field, field_hash
+    field.fields.each do |subfield|
+      if !is_in_hash(subfield, field_hash[field.name])
+        puts "Missing #{field.name}.#{subfield.name}"
+        return false
+      end
+    end
+    true
+  end
+
+  def check_array_subfields field, field_hash
+    field.fields.each do |subfield|
+      if !is_in_hash(subfield, field_hash[field.name][0])
+        puts "Missing #{field.name}.#{subfield.name}"
+        return false
+      end
+    end
+    true
+  end
+
+  def is_in_hash(field, field_hash)
+    if field_hash.class.to_s != "Hash" || field_hash.empty?
+      return false
+    end
+
+    if field_hash.has_key?(field.name)
+      if !field.has_subfields?
+        return true
+      else
+        return true
+      end
+    end
+    false
+  end
+
+  def check_based_on_type field, field_hash
+    field.is_a?(ObjectField) ? check_object_subfields(field, field_hash) : check_array_subfields(field, field_hash)
   end
 end
--- spec/tester/field_checker_spec.rb
+++ spec/tester/field_checker_spec.rb
@@ -25,7 +25,7 @@ describe FieldChecker do
     end
   end
 
-  context 'subfields' do
+  context 'object fields' do
     it 'should handle object fields' do
       subfield = Field.new "subField"
       field = ObjectField.new("objectField").with_field(subfield)
@@ -38,6 +38,36 @@ describe FieldChecker do
       field = ObjectField.new("objectField").with_field(subfield)
       hash = { "objectField" => {"notSubField" => 1} }
       expect(fieldChecker.is_field_in_hash(field, hash)).to be false
+    end
+  end
+
+  context 'array fields' do
+    it 'should handle array fields' do
+      subfield = Field.new "subField"
+      field = ArrayField.new("arrayField").with_field(subfield)
+      hash = { "arrayField" => [{"subField" => 1}] }
+      expect(fieldChecker.is_field_in_hash(field, hash)).to be true
+    end
+
+    it 'should be able to identify when array is missing subfields' do
+      subfield = Field.new "subField"
+      field = ArrayField.new("arrayField").with_field(subfield)
+      hash = { "arrayField" => [{"notSubField" => 1}] }
+      expect(fieldChecker.is_field_in_hash(field, hash)).to be false
+    end
+  end
+
+  context '#check_object_subfields' do
+    it 'should id subfields in hash passed' do
+      field = ObjectField.new("obj").with_field(Field.new "subField")
+      field_hash = {"obj" => {"subField" => "foo"}}
+      expect(fieldChecker.check_object_subfields(field, field_hash)).to be true
+    end
+
+    it 'should fail if missing the subfield' do
+      field = ObjectField.new("obj").with_field(Field.new "subField")
+      field_hash = {"obj" => {"not_sub_field" => "foo"}}
+      expect(fieldChecker.check_object_subfields(field, field_hash)).to be false
     end
   end
 end
