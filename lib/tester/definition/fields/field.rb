c20a5acdf6fbc7dd102e7a7946a8e53265cdcb27
Sarah <sarahforseti@gmail.com>
2017-04-30 17:51:08 -0400

    Expanding fields to be set to false or true

--- lib/tester/definition/fields/field.rb
+++ lib/tester/definition/fields/field.rb
@@ -1,10 +1,22 @@
 class Field
   attr_accessor :name
   attr_accessor :default_value
+  attr_accessor :required
 
   def initialize name, default_value="string"
     self.name = name
     self.default_value = default_value
+    self.required = false
+  end
+
+  def is_required
+    self.required = true
+    self
+  end
+
+  def is_not_required
+    self.required = false
+    self
   end
 
   def has_subfields?
@@ -12,6 +24,10 @@ class Field
   end
 
   def negative_boundary_values
-    []
+    cases = []
+    if self.required
+      cases << nil
+    end
+    cases
   end
 end
--- spec/tester/definition/fields/field_spec.rb
+++ spec/tester/definition/fields/field_spec.rb
@@ -2,15 +2,35 @@ require "spec_helper"
 require 'tester/definition/fields/field'
 
 describe Field do
-  context "default value" do
-    it 'defaults to "string"' do
+  context 'default params' do
+    it 'defaults required to false' do
+      expect(Field.new("testObj").required).to be false
+    end
+
+    it 'defaults default_value to "string"' do
       field = Field.new("testObj")
       expect(field.default_value).to eq "string"
     end
+  end
 
+  context "default value" do
     it "can be set" do
       field = Field.new("testObj", "new foo")
       expect(field.default_value).to eq "new foo"
+    end
+  end
+
+  context 'required field' do
+    it 'has a nil negative boundary case' do
+      field = Field.new("testOb").is_required
+      expect(field.negative_boundary_values).to include nil
+    end
+  end
+
+  context 'not required field' do
+    it 'does not have a nil negative boundary case' do
+      field = Field.new("testOb").is_not_required
+      expect(field.negative_boundary_values).not_to include nil
     end
   end
 end
