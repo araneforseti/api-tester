6d16faf29bf67f170aeaa89f880ed1d1eb71be10
Sarah <sarahforseti@gmail.com>
2017-10-21 23:26:56 -0500

    Changing spacing

--- lib/tester/modules/good_case.rb
+++ lib/tester/modules/good_case.rb
@@ -2,19 +2,19 @@ require 'tester/reporter/status_code_report'
 require 'tester/modules/module'
 
 class GoodCase < Module
-  def go definition, report
-    super
+    def go definition, report
+        super
 
-    definition.methods.each do |method|
-      payload = method.request.default_payload
-      response = method.call payload, method.request.default_headers
-      self.response_matches(response, payload, method.expected_response, method, "good case")
+        definition.methods.each do |method|
+            payload = method.request.default_payload
+            response = method.call payload, method.request.default_headers
+            self.response_matches(response, payload, method.expected_response, method, "good case")
+        end
+
+        report.reports == []
     end
 
-    report.reports == []
-  end
-
-  def order
-    1
-  end
+    def order
+        1
+    end
 end
