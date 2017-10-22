d7e8426bb723975f3ac1945cc75507d706003814
Sarah <sarahforseti@gmail.com>
2017-10-22 16:11:09 -0500

    Updating indentation

--- spec/tester/definition/response_spec.rb
+++ spec/tester/definition/response_spec.rb
@@ -2,24 +2,25 @@ require "spec_helper"
 require 'tester/definition/response'
 
 describe Response do
-  let(:code) {200}
-  let(:response) {Response.new code}
+    let(:code) {200}
+    let(:response) {Response.new code}
 
-  context "code" do
-    it "starts has status_cpde" do
-      expect(response.code).to eq code
-    end
-  end
-
-  context 'fields' do
-    it "starts with no fields" do
-      expect(response.body).to eq []
+    context "code" do
+        it "starts has status_cpde" do
+            expect(response.code).to eq code
+        end
     end
 
-    it "can add fields" do
-      response.add_field Field.new("newField")
-      expect(response.body.size).to be 1
-      expect(response.body.first.name).to eq "newField"
+    context 'fields' do
+        it "starts with no fields" do
+            expect(response.body).to eq []
+        end
+
+        it "can add fields" do
+            response.add_field Field.new("newField")
+            expect(response.body.size).to be 1
+            expect(response.body.first.name).to eq "newField"
+        end
     end
-  end
+
 end
