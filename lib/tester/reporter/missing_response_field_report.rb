46edddae2126a1808a6b2ca1384843d5425e7501
Sarah <sarahforseti@gmail.com>
2018-05-02 22:04:22 -0300

    Improving output of unused fields module

--- lib/tester/reporter/missing_response_field_report.rb
+++ lib/tester/reporter/missing_response_field_report.rb
@@ -1,16 +1,19 @@
 class MissingResponseFieldReport
   attr_accessor :url
+  attr_accessor :verb
   attr_accessor :expected_field
   attr_accessor :description
 
-  def initialize(url, expected_field, description)
+  def initialize(url, verb, expected_field, description)
     self.url = url
+    self.verb = verb
     self.expected_field = expected_field
     self.description = description
   end
 
   def print
-    puts "#{self.description} - #{self.url} is missing response field:"
+    puts "#{self.description}:"
+    puts "   #{self.verb} #{self.url} is missing response field:"
     puts "      #{self.expected_field}"
   end
 end
